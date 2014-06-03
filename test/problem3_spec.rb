d621a24cab2817f4ffdf511ea51a7a7259c1f0e9
Paul Haddad <paulh16@gmail.com>
2014-06-03 18:31:15 +0100

    Completion of Problem 3

--- test/problem3_spec.rb
+++ test/problem3_spec.rb
@@ -4,4 +4,10 @@ describe '#largest_prime_factor' do
   it 'returns 29 when the given number is 13195' do
     expect(largest_prime_factor(13195)).to eq(29)
   end
+
+  it 'returns 6857 when the given number is 13195' do
+    expect(largest_prime_factor(600851475143)).to eq(6857)
+  end
+
+
 end
